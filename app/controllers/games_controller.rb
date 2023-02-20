a9791a61b24fe17751eaca6bbf36f442ea884121
Robbie Stanley-Smith <robbie.stanleysmith@gmail.com>
2023-02-20 18:23:05 +0000

    done after comparing with solution and editing

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,36 +8,22 @@ class GamesController < ApplicationController
   end
 
   def score
-    @word = params[:user_guess]
-    @letters = params[:letters]
-    @english_word = is_english_word?(@word)
+    @word = params[:user_guess].upcase
+    @letters = params[:letters].split
+    @english_word = english_word?(@word)
     @formable_word = word_can_be_formed_from_letters?(@word, @letters)
-    # if @word can be formed from @letters && @word is_english_word?
-    # @response = "Congratulations! @word is an English word!"
-    # elsif is_english_word?
-    # @response = "Sorry, @word can't be built out of params[:letters]"
-    # elsif word_can_be_formed_from_letters?
-    # @response = "Sorry, @word doesn't seem to be a valid English word.."
-    # else
-    # @response = "Sorry, @word isn't an English word and can't be built out of params[:letters] either!"
   end
+
+  private
 
   def word_can_be_formed_from_letters?(word, letters)
-    each_letter_of_word = word.split('')
-    each_letter_of_word.each do |letter| {
-      word_count = letter.count
-
-    }
-
-
-
-
+    @included = word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
   end
 
-  def is_english_word?(word)
+  def english_word?(word)
     url = "https://wagon-dictionary.herokuapp.com/#{word}"
     html_file = URI.open(url).read
-    @results_hash = JSON.parse(html_file)
-    return @results_hash[:found]
+    json = JSON.parse(html_file)
+    json['found']
   end
 end
